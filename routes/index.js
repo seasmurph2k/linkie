1ac934f352cb3ca3507a5500c0a87b244f2752bc
Connor Murphy <dev.web@engineer.com>
2019-04-02 07:13:28 +0100

    Initial rough sketch of a dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
-var express = require("express");
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index");
 });
+
 router.get("/register", (req, res) => {
   res.render("register");
 });
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get(
+  "/dashboard",
+  (req, res, next) => {
+    req.isAuthenticated() ? next() : res.redirect("/login");
+  },
+  (req, res) => {
+    res.render("dashboard");
+  }
+);
+
 module.exports = router;
