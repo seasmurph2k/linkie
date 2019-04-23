8cb25b6bb0d2e7a5e6e23d4e372dad57306b10c0
Connor Murphy <dev.web@engineer.com>
2019-04-23 07:24:45 +0100

    Conditionally render navbar & added logout route

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,25 @@ const router = express.Router();
 const passport = require("passport");
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index");
+  res.render("index", {
+    isAuthed: req.isAuthenticated()
+  });
 });
 
 router.get("/register", (req, res) => {
-  res.render("register");
+  res.render("register", {
+    isAuthed: req.isAuthenticated()
+  });
 });
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", {
+    isAuthed: req.isAuthenticated()
+  });
+});
+router.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/");
 });
 
 router.get(
